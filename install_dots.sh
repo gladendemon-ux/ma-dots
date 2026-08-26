2a8b1d82e692919bdc93b83e869f39cda1ddff03
gladendemon-ux <gladendemon@gmail.com>
2026-08-26 09:40:19 +0300

    added installer script

---
+++ install_dots.sh
@@ -0,0 +1,31 @@
+mkdir -p ~/.config/eww/
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/eww ~/.config/eww/
+mkdir -p ~/.config/hypr/
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/hypr ~/.config/hypr/
+mkdir -p ~/.config/kitty/
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/kitty ~/.config/kitty/
+mkdir -p ~/.config/vim/
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/vim ~/.config/vim/
+mkdir -p ~/wallpaper/
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/wallpapers ~/.config/wallpapers/
+mkdir -p ~/wlogout-icons/
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/wlogout-icons ~/.config/wlogout-icons/
+mkdir -p ~/wlogout/
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/wlogout ~/.config/wlogout/
+mkdir -p ~/wofi/
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/wofi ~/.config/wofi/
+
+
+touch cover
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/exitPeek.sh
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/get-cover.sh
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/getsongcover.sh
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/opacityZero.sh
+
+
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/vimrc
+mv vimrc ~/.vimrc
+
+git clone https://github.com/gladendemon-ux/ma-dots/tree/main/zshrc
+mv vimrc ~/.zshrc
+
